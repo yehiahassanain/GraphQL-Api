bfc8c9baaebc57107a68618aa07541d5f71d69cb
yehiahassanain <89047698+yehiahassanain@users.noreply.github.com>
2024-03-05 03:36:16 +0200

    working with Rest API-The practical Application

--- Rest-api/app.js
+++ Rest-api/app.js
@@ -1,18 +1,69 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const path = require('path');
 
-const feedRoutes = require('./routes/feet')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const multer = require('multer');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
 const app = express();
 
-// app.use(bodyParser.urlencoded())//x-www-form-urlencoded
-app.use(bodyParser.json());//application/json
-
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods','OPTION, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
-    next();
+const { v4: uuidv4 } = require('uuid');
+ 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuidv4() + '-' + file.originalname);
+    }
 });
-app.use('/feed',feedRoutes)
 
-app.listen(8080);
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data:data });
+});
+
+mongoose
+        .connect( 'mongodb+srv://yehiahassanain:efoszQPFvYVZGA8o@cluster0.j3razmw.mongodb.net/messages' ) 
+          .then(result => { 
+            app.listen(8080);
+           }) 
+          .catch(err => console.log(err));
--- Rest-api/routes/feet.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const feedController = require('../controllers/feed')
-const router = express.Router();
-// Get/feet/posts
-router.get('/posts',feedController.getPosts);
-// post/feet/posts
-router.post('/post',feedController.createPost);
-module.exports = router;
